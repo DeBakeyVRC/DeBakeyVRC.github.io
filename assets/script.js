68d78f74534b6af4ad00517053e780914163e00e
jkohhokj <50003626+jkohhokj@users.noreply.github.com>
2022-05-24 20:33:15 -0500

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,17 @@
-let test = document.getElementById("hl");
+container.onmouseover = container.onmouseout = handler;
 
-// This handler will be executed only once when the cursor
-// moves over the unordered list
-test.addEventListener("mouseenter", function( event ) {
-  // highlight the mouseenter target
-  event.target.style.color = "purple";
+function handler(event) {
 
-  // reset the color after a short delay
-  setTimeout(function() {
-    event.target.style.color = "";
-  }, 500);
-}, false);
+  function str(el) {
+    if (!el) return "null"
+    return el.className || el.tagName;
+  }
+  if (el == "hl"){
+    if (event.type == 'mouseover') {
+      event.target.style.background = 'purple'
+    }
+    if (event.type == 'mouseout') {
+      event.target.style.background = ''
+    }
+  }
+}
